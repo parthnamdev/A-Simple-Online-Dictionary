0dc6d24aea5f99fe418411fe40d96c697410ba5f
parthnamdev <parthnamdev@users.noreply.github.com>
2020-05-13 23:11:22 +0530

    output page started

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.render("index");
+    res.render("output");
 });
 app.listen(3000, function(){
     console.log("server started on port 3000.");
